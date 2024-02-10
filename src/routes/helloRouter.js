42e71e668ef64139577ccd953ee44e893961c2f1
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-02-10 12:55:00 +0200

    add /hello rout

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -1,9 +1,36 @@
 import { Router } from "express";
+import axios from 'axios';
 
 const routes = Router();
 
-routes.get("/", async (req, res) => {
-  res.status(200).send({ message: "Hello Maks" });
+routes.post("/hello", async (req, res) => {
+  try {
+    const orderData = req.body;
+    const telegramBotToken = '6769576713:AAFHPH_ObAOeNGC9kvYNWt8mG8-utwPs7KQ';
+    const chatId = '1936815365';
+
+    const message = `
+      Новый заказ!
+      Имя: ${orderData.userName}
+      Телефон: ${orderData.userTelephone}
+      Город: ${orderData.deliveryCityName}
+      Отделение: ${orderData.deliveryDepart}
+      Имя Товара ${orderData.userOrderProduct}
+      Размеры ${orderData.productSizes}
+      Оплата: ${orderData.paymentOption}
+      Комментарий: ${orderData.orderComment}
+    `;
+
+    await axios.post(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+      chat_id: chatId,
+      text: message,
+    });
+
+    res.status(200).send('Order submitted successfully');
+  } catch (error) {
+    console.error('Error submitting order:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 export default routes;
