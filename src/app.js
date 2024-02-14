03147f0e189f66b485db638ba38708467f44d2fb
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-02-14 11:21:45 +0200

    logo

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import morgan from "morgan";
 
 import * as middleware from "./utils/middleware.js";
-import helloRoute from "./routes/helloRouter.js";
 
 const app = express();
 
@@ -21,7 +20,6 @@ app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
-app.use("/hello", helloRoute);
 
 // custom middleware
 app.use(middleware.unknownEndpoint);
