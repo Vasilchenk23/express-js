4a7d1acea78600fa28fd28702461bd071eb16e77
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-02-14 11:33:15 +0200

    bot

--- src/app.js
+++ src/app.js
@@ -1,25 +1,56 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import axios from "axios";
+import TelegramBot from "node-telegram-bot-api";
 
 import * as middleware from "./utils/middleware.js";
 
 const app = express();
+const botToken = '6769576713:AAFHPH_ObAOeNGC9kvYNWt8mG8-utwPs7KQ';
+const bot = new TelegramBot(botToken);
 
-// parse json request body
+
 app.use(express.json());
 
-// enable cors
+
 app.use(cors());
 
-// request logger middleware
+
 app.use(morgan("tiny"));
 
-// healthcheck endpoint
+
 app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
+
+app.post("/order", (req, res) => {
+  const { userName, userTelephone, deliveryCityName, deliveryDepart, userOrderProduct, productSizes, paymentOption, orderComment } = req.body;
+
+
+  const message = `
+    Новый заказ:
+    Имя: ${userName}
+    Телефон: ${userTelephone}
+    Город: ${deliveryCityName}
+    Отделение: ${deliveryDepart}
+    Продукт: ${userOrderProduct}
+    Размер: ${productSizes}
+    Оплата: ${paymentOption}
+    Комментарий: ${orderComment}
+  `;
+
+  bot.sendMessage('1936815365', message)
+    .then(() => {
+      console.log('Order notification sent to Telegram');
+      res.status(200).send({ message: 'Order submitted successfully' });
+    })
+    .catch((error) => {
+      console.error('Error sending order notification to Telegram:', error);
+      res.status(500).send({ error: 'Failed to submit order' });
+    });
+});
 
 // custom middleware
 app.use(middleware.unknownEndpoint);
